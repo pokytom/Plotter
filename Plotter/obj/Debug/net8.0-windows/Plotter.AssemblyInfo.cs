feb8eb7e021ef53719815a77ce74a9f3708cd8df
pokytom <pokytom@seznam.cz>
2025-03-27 17:46:35 +0100

    Example plot

--- Plotter/obj/Debug/net8.0-windows/Plotter.AssemblyInfo.cs
+++ Plotter/obj/Debug/net8.0-windows/Plotter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plotter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63bb5824197741233431458a749d56ad83484478")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plotter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plotter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
